cef8e8a16918b8421e8cff4b3bd5dc511e8fb2d9
Matt Soucoup <msoucoup@codemilltech.com>
2018-05-30 09:22:06 -0500

    Initial commit

---
+++ src/ToDoItems.Core/Helpers/APIKeys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ToDoItems.Core
+{
+    public class APIKeys
+    {
+        public APIKeys()
+        {
+        }
+
+#error Enter the URL of your Azure Cosmos DB endpoint here
+        public static readonly string CosmosEndpointUrl = "";
+
+#error Enter the read/write authentication key of your Azure Cosmos DB endpoint here
+        public static readonly string CosmosAuthKey = "";
+    }
+}
